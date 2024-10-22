5902b48dd08d9818a3b7b121a87390e6be9e48ae
laxmis-prog <laxmisanuu5@gmail.com>
2024-10-22 20:15:51 +0300

    Added head.php

---
+++ head.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="fi">
+  <head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Hyvinvointiseuranta</title>
+    <link rel="stylesheet" href="styles.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      rel="stylesheet"
+    />
+
+  </head>
+  <body>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous"
+    ></script>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+     <div class="container-fluid">
+       <a class="navbar-brand" href="Kotisivu.php">
+         <img src="logoo.png" alt="Hyvinvointiseuranta Logo" class="img-fluid" style="height: 60px; width: auto;" />
+       </a>
+
+
+       <button
+         class="navbar-toggler"
+         type="button"
+         data-bs-toggle="collapse"
+         data-bs-target="#navbarNav"
+         aria-controls="navbarNav"
+         aria-expanded="false"
+         aria-label="Toggle navigation"
+       >
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarNav">
+         <ul class="navbar-nav ms-auto">
+           <li class="nav-item">
+             <a
+               class="nav-link active"
+               aria-current="page"
+               href="Kotisivu.php"
+             >
+               <i class="fa fa-home" aria-hidden="true"></i>
+               Kotisivu</a
+             >
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="Yhteystiedot.php">
+               <i class="fa fa-envelope" aria-hidden="true"></i>
+               Yhteystiedot</a
+             >
+
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Kirjaudu.php">
+                <i class="fa fa-user" aria-hidden="true"></i>Kirjaudu</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Rekisteroidy.php">
+                <i class="fa fa-user-plus" aria-hidden="true"></i>
+                Rekisteröidy</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    </div>
+
+  </body>
+</html>
